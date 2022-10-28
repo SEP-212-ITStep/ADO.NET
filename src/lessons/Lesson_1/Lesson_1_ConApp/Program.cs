b9997775d54a54d108b71980fc0591288ff1693e
Egor Sidorenko <sidorenkoegor1999@mail.ru>
2022-10-28 16:55:23 +0300

    Add project files.

---
+++ src/lessons/Lesson_1/Lesson_1_ConApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lesson_1_ConApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
