7e5000b2d3daf45f7e264732ad7c708e169c727d
Egor Sidorenko <sidorenkoegor1999@mail.ru>
2022-10-28 17:35:48 +0300

    added home work

---
+++ src/home-works/Lesson_2/SidorenkoEgor/SidorenkoEgor_Lesson_2_Home_Work/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SidorenkoEgor_Lesson_2_Home_Work
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
