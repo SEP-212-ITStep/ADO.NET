aeb402ebbb428317fab5b1208d94a93688e077f5
Egor Sidorenko <sidorenkoegor1999@mail.ru>
2022-11-04 18:05:53 +0300

    added new menu

--- src/lessons/Lesson_4/ef-core/Program.cs
+++ src/lessons/Lesson_4/ef-core/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace ef_core
+﻿using System.ComponentModel;
+
+namespace ef_core
 {
     internal class Program
     {
@@ -8,39 +10,57 @@
             // 2. Обновить контент блога
             // 3. Удалить
             // 4. Показать всё
-            Console.WriteLine("1. Добавить блог");
-            Console.WriteLine("2. Обновить контент блога");
-            Console.WriteLine("3. Удалить");
-            Console.WriteLine("4. Показать всё");
+           
             try
             {
-                var inputString = Console.ReadLine();
-                if (string.IsNullOrEmpty(inputString))
-                {
-                    return;
-                }
-
-                int number = int.Parse(inputString);
-                switch (number)
-                {
-                    case 1:
-                        break;
-                    case 2:
-                        break;
-                    case 3:
-                        break;
-                    case 4:
-                        ShowAllData();
-                        break;
-                    default:
-                        break;
-                }
+                var number = ShowMenu();
+                SwitchMenu(number);
             }
             catch (Exception e)
             {
                 Console.WriteLine(e);
             }
         }
+        public static void SwitchMenu(int number)
+        {
+            switch (number)
+            {
+                case 1:
+                    Console.Write("Имя автора:");
+                    var authorName = Console.ReadLine();
+                    Console.Write("Контент блога:");
+                    var blogContent = Console.ReadLine();
+                    AddNewBlog(authorName, blogContent);
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    ShowAllData();
+                    break;
+                default:
+                    break;
+            }
+        }
+        public static int ShowMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("1. Добавить блог");
+            Console.WriteLine("2. Обновить контент блога");
+            Console.WriteLine("3. Удалить");
+            Console.WriteLine("4. Показать всё");
+
+            var inputString = Console.ReadLine();
+            if (string.IsNullOrEmpty(inputString))
+            {
+                return 0;
+            }
+
+            int number = int.Parse(inputString);
+            return number;
+        }
+
         public static void AddNewBlog(string authorName, string content)
         {
             var blog = new Blog()
@@ -55,6 +75,9 @@
 
             dbContext.Blogs.Add(blog);
             dbContext.SaveChanges();
+            Console.WriteLine("Изменения успешно сохранены.");
+            Thread.Sleep(1500);
+            SwitchMenu(ShowMenu());
         }
         public static void ShowAllData()
         {
