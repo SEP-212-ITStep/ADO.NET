f44df28d50901dd89ea029e15d4214eb1003acee
Assir Abdukhalikov <asirushtomblack@gmail.com>
2022-11-08 20:02:19 +0600

    homework 1

---
+++ connectedMode/DbService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace connectedMode
+{
+    internal class DbService
+    {
+        public bool ConnectDb(string ConnString)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(ConnString))
+                {
+                    connection.Open();
+                }
+                Console.WriteLine("Connection successfull");
+                return true;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Connection refused! Error:", ex);
+                return false;
+            }
+        }
+    }
+}
---
+++ connectedMode/product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace connectedMode
+{
+    internal class product
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public provider Provider { get; set; }
+        public int Count { get; set; }
+        public double Cost { get; set; }
+        public DateTime GetDate { get; set; }
+
+
+    }
+}
