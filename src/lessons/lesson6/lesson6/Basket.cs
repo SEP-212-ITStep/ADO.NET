500168e5485653807eb9b5ecc764f5251aef7f51
Nfiec <95955366+Nfiec@users.noreply.github.com>
2022-11-27 12:28:47 +0600

    Add files via upload

---
+++ src/lessons/lesson6/lesson6/Basket.cs
@@ -0,0 +1,20 @@
+﻿using lesson6;
+using System;
+using System.Collections.Generic;
+
+namespace Db_First_Approach.Models;
+
+public partial class Basket
+{
+    public int Id { get; set; }
+
+    public int UserId { get; set; }
+
+    public DateTime CreateDate { get; set; }
+
+    public DateTime? UpdateDate { get; set; }
+
+    public virtual ICollection<BasketProduct> BasketProducts { get; } = new List<BasketProduct>();
+
+    public virtual User User { get; set; } = null!;
+}
