5320e72055b16358572434de4c1e4c7142ad54d1
Nfiec <95955366+Nfiec@users.noreply.github.com>
2022-11-27 12:28:05 +0600

    Add files via upload

---
+++ src/lessons/Lesson5/Lesson5/PersonAddress.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace namespace Lesson5
+
+{
+    public class PersonAddress
+    {
+        public int Id { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        public int PostCode { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+
+    }
+}
---
+++ src/lessons/Lesson5/Lesson5/PersonDBContext.cs
@@ -0,0 +1,24 @@
+﻿using .Lesson5;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using Lesson5;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lesson5
+{
+    public class PersonDbContext : DbContext
+    {
+        public DbSet<PersonAddress> PeopleAddresses { get; set; }
+        public DbSet<Person> People { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            builder.UseSqlServer("Server=LAPTOP-EGOR\\EGOR_SQL_SERVER;Database=PeopleDb;" +
+                                 "Trusted_Connection=true;Encrypt=false");
+
+        }
+    }
+}
