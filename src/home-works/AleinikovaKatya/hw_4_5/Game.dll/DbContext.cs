48a6c94d8353788ada9fbbe22eb27fc5294210e2
UserKatya <User@DESKTOP-D9GJV4L>
2022-11-28 17:54:53 +0600

    hw_4_5

---
+++ src/home-works/AleinikovaKatya/hw_4_5/Game.dll/DbContext.cs
@@ -0,0 +1,39 @@
+﻿using HWGame.dll;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HWGame.dll
+{
+    public class GameContext : DbContext
+    {
+        public DbSet<Model> Games { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            builder.UseSqlServer("Server=DESKTOP-D9GJV4L\\MSSQLSERVER01;Database=GameDb;Trusted_Connection=true;Encrypt=false");
+
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<Model>().HasData(new List<Model>()
+            {
+                new Model()
+                {
+                   Id = 1,
+                   GameName = "Dead by Daylight",
+                   GameDeveloper = "Behaviour Interactive",
+                   GameStyle = "Horror",
+                   GameMode = "Surviver",
+                   GameCells = 3000000,
+                   GamePublishYear = 2015
+                }
+            });
+        }
+    }
+}
---
+++ src/home-works/AleinikovaKatya/hw_4_5/Game.dll/Model.cs
@@ -0,0 +1,15 @@
+﻿namespace HWGame.dll
+{
+    public class Model
+    {
+        public int Id { get; set; }
+        public string GameDeveloper { get; set; }
+        public string GameName { get; set; }
+        public string GameStyle { get; set; }
+        public string GameMode { get; set; }
+        public int GameCells { get; set; }
+        public int GamePublishYear { get; set; }
+
+
+    }
+}
